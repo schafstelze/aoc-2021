fe7534f23646734397ac47d46a2416ef35cf6374
athierba <andre.thierbach@web.de>
2021-12-01 08:50:49 +0100

    Solution day01

--- day01/solutionDay01.spec.ts
+++ day01/solutionDay01.spec.ts
@@ -1,7 +1,28 @@
-import {solutionDay01} from "./solutionDay01";
+import {solutionDay01Part1, solutionDay01Part2} from "./solutionDay01";
+const fs = require('fs');
+
+const complexInput: number[] = fs.readFileSync('day01/input.txt', 'utf8')
+    .split(/\r?\n/)
+    .map((item: string) => Number(item))
 
 describe('solution.ts', () => {
-    it('should be correct', async () => {
-        expect(solutionDay01()).toBeTruthy()
+    it('solution part1 should be correct on simple input', async () => {
+        const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+        expect(solutionDay01Part1(input)).toBe(7)
+    })
+
+    it('solution part2 should be correct on complex input', async () => {
+        expect(solutionDay01Part1(complexInput)).toBe(1298)
+    })
+
+    it('solution part2 should be correct on simple input', async () => {
+        const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+        expect(solutionDay01Part2(input)).toBe(5)
+    })
+
+    it('solution part2 should be correct on complex input', async () => {
+        expect(solutionDay01Part2(complexInput)).toBe(1248)
     })
 })
--- day01/solutionDay01.ts
+++ day01/solutionDay01.ts
@@ -1,5 +1,31 @@
-export const solutionDay01 = (): boolean => {
-    console.log("drop your stuff here")
+export const solutionDay01Part1 = (measurements: number[]): number => {
+    let numberOfIncreases = 0
 
-    return true
+    for (let index = 0; index < measurements.length; index++) {
+        if (index == 0) {
+            continue
+        }
+
+        if (measurements[index] - measurements[index - 1] > 0) {
+            numberOfIncreases++
+        }
+    }
+
+    return numberOfIncreases
+}
+
+export const solutionDay01Part2 = (measurements: number[]): number => {
+    let numberOfIncreases = 0
+
+    for (let index = 0; index < measurements.length - 3; index++) {
+        const firstWindow: number = measurements[index] + measurements[index + 1] + measurements[index + 2]
+        const secondWindow: number = measurements[index + 1] + measurements[index + 2] + measurements[index + 3]
+
+        if (secondWindow - firstWindow > 0) {
+            numberOfIncreases++
+        } else {
+        }
+    }
+
+    return numberOfIncreases
 }
