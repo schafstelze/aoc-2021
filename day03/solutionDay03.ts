be366104b1f4e38cbaa3a787a45d138748260d4a
athierba <andre.thierbach@web.de>
2021-12-03 11:37:52 +0100

    refactor day03

--- day03/solutionDay03.ts
+++ day03/solutionDay03.ts
@@ -1,18 +1,32 @@
+type DigitCount = {
+    zeros: number
+    ones: number
+}
+
+const countDigits = (sequences: string[], index: number): DigitCount => {
+    const digitCount: DigitCount = {
+        zeros: 0,
+        ones: 0
+    }
+
+    sequences.forEach(sequence => {
+        if (sequence.charAt(index) === '0') {
+            digitCount.zeros++
+        } else {
+            digitCount.ones++
+        }
+    })
+
+    return digitCount
+}
+
 export const solutionDay03Part1 = (sequences: string[]): number => {
     let gammaRate: string = "", epsilonRate: string = ""
 
     for (let index = 0; index < sequences[0].length; index++) {
-        let countOfZeros = 0
-        let countOfOnes = 0
+        const digitCount = countDigits(sequences, index)
 
-        sequences.forEach(sequence => {
-            if (sequence.charAt(index) === '0') {
-                countOfZeros++
-            } else {
-                countOfOnes++
-            }
-        })
-        if (countOfZeros > countOfOnes) {
+        if (digitCount.zeros > digitCount.ones) {
             gammaRate += "0"
             epsilonRate +="1"
         } else {
@@ -21,54 +35,31 @@ export const solutionDay03Part1 = (sequences: string[]): number => {
         }
     }
 
-    console.log(`gammaRate: ${gammaRate}, epsilonRate: ${epsilonRate}`)
-
     return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
 }
 
 export const solutionDay03Part2 = (sequences: string[]): number => {
     let oxygenRates: string[] = sequences.slice(), co2Rates: string[] = sequences.slice()
 
-    let index = 0
-    while (oxygenRates.length > 1) {
-        let countOfZeros = 0
-        let countOfOnes = 0
+    for (let index = 0; index < sequences[0].length; index++) {
+        const digitCount = countDigits(oxygenRates, index)
 
-        oxygenRates.forEach(sequence => {
-            if (sequence.charAt(index) === '0') {
-                countOfZeros++
-            } else {
-                countOfOnes++
-            }
-        })
-        if (countOfZeros > countOfOnes) {
-            oxygenRates = oxygenRates.filter(rate => rate.charAt(index) === '0')
-        } else {
-            oxygenRates = oxygenRates.filter(rate => rate.charAt(index) === '1')
-        }
-        index++
+        const digitToFilter: string = digitCount.zeros > digitCount.ones ? '0' : '1'
+
+        oxygenRates = oxygenRates.filter(rate => rate.charAt(index) === digitToFilter)
+
+        if (oxygenRates.length === 1) break
     }
 
-    index = 0
-    while (co2Rates.length > 1) {
-        let countOfZeros = 0
-        let countOfOnes = 0
+    for (let index = 0; index < sequences[0].length; index++) {
+        const digitCount = countDigits(co2Rates, index)
 
-        co2Rates.forEach(sequence => {
-            if (sequence.charAt(index) === '0') {
-                countOfZeros++
-            } else {
-                countOfOnes++
-            }
-        })
-        if (countOfZeros > countOfOnes) {
-            co2Rates = co2Rates.filter(rate => rate.charAt(index) === '1')
-        } else {
-            co2Rates = co2Rates.filter(rate => rate.charAt(index) === '0')
-        }
-        index++
+        const digitToFilter: string = digitCount.zeros > digitCount.ones ? '1' : '0'
+
+        co2Rates = co2Rates.filter(rate => rate.charAt(index) === digitToFilter)
+
+        if (co2Rates.length === 1) break
     }
-    console.log(`oxygenRates: ${oxygenRates}, co2Rates: ${co2Rates}`)
 
     return parseInt(oxygenRates[0], 2) * parseInt(co2Rates[0], 2)
 }
